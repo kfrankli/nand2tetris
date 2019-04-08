832915c6615a37f9e890c93bb5ad0aee4f3ea9d7
Kevin Franklin <kfrankli@redhat.com>
2019-04-08 09:18:44 -0400

    Updated assembler to cover jump and dest table maps

--- projects/06/src/Assembler.java
+++ projects/06/src/Assembler.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 public class Assembler {
   public static HashMap<String,String> compAddressInstructionMap = new HashMap<String, String>();
   public static HashMap<String,String> compComputeInstructionMap = new HashMap<String, String>();
+
+  public static HashMap<String,String> destMap = new HashMap<String, String>();
+  public static HashMap<String,String> jumpMap = new HashMap<String, String>();
+
   public static HashMap<String,Integer> predefinedSymbolsMap = new HashMap<String, Integer>();
 
   static {
@@ -16,6 +20,7 @@ public class Assembler {
      *
      * comp |                   | comp
      * a=0  | c1 c2 c3 c4 c5 c6 | a=1
+     * -------------------------------
      *  0   | 1  0  1  0  1  0  |
      *  1   | 1  1  1  1  1  1  |
      * -1   | 1  1  1  0  1  0  |
@@ -35,6 +40,8 @@ public class Assembler {
      * D&A  | 0  0  0  0  0  0  |  D&M
      * D|A  | 0  1  0  1  0  1  |  D|M
      */
+
+    // Address Instruction table
     compAddressInstructionMap.put("0","101010");
     compAddressInstructionMap.put("1","111111");
     compAddressInstructionMap.put("-1","111010");
@@ -54,10 +61,69 @@ public class Assembler {
     compAddressInstructionMap.put("D&A","000000");
     compAddressInstructionMap.put("D|A","010101");
 
+    // Compute Instruction table
+    //put all comp posibilities with M into a HashMap,a=1
+    compComputeInstructionMap.put("M","110000");
+    compComputeInstructionMap.put("!M","110001");
+    compComputeInstructionMap.put("-M","110011");
+    compComputeInstructionMap.put("M+1","110111");
+    compComputeInstructionMap.put("M-1","110010");
+    compComputeInstructionMap.put("D+M","000010");
+    compComputeInstructionMap.put("D-M","010011");
+    compComputeInstructionMap.put("M-D","000111");
+    compComputeInstructionMap.put("D&M","000000");
+    compComputeInstructionMap.put("D|M","010101");
+
+    /*
+     * Specified by 6.2.2 Instructions, page 110
+     *
+     *  dest |  d1 d2 d3
+     *  ----------------
+     *  null |  0  0  0
+     *     M |  0  0  1
+     *     D |  0  1  0
+     *    MD |  0  1  1
+     *     A |  1  0  0
+     *    AM |  1  0  1
+     *    AD |  1  1  0
+     *   AMD |  1  1  1
+     */
+    destMap.put("","000");
+    destMap.put("M","001");
+    destMap.put("D","010");
+    destMap.put("MD","011");
+    destMap.put("A","100");
+    destMap.put("AM","101");
+    destMap.put("AD","110");
+    destMap.put("AMD","111");
+
+    /*
+     * Specified by 6.2.2 Instructions, page 110
+     *  jump |  j1 j2 j3
+     *  ----------------
+     *  null |  0  0  0
+     *   JGT |  0  0  1
+     *   JEQ |  0  1  0
+     *   JGE |  0  1  1
+     *   JLT |  1  0  0
+     *   JNE |  1  0  1
+     *   JLE |  1  1  0
+     *   JMP |  1  1  1
+     */
+    jumpMap.put("","000");
+    jumpMap.put("JGT","001");
+    jumpMap.put("JEQ","010");
+    jumpMap.put("JGE","011");
+    jumpMap.put("JLT","100");
+    jumpMap.put("JNE","101");
+    jumpMap.put("JLE","110");
+    jumpMap.put("JMP","111");
+
     /*
      * Specified by section 6.2.3 Symbols, page 110
      *
      * Label  | RAM | address (hexa)
+     * -----------------------------
      *  SP    | 0   | 0x0000
      *  LCL   | 1   | 0x0001
      *  ARG   | 2   | 0x0002
@@ -90,6 +156,7 @@ public class Assembler {
     predefinedSymbolsMap.put("R15",15);
     predefinedSymbolsMap.put("SCREEN",16384);
     predefinedSymbolsMap.put("KBD",24576);
+
   }
   public static void main(String[] args) {
     if (args.length == 0) {
